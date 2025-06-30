b07e93a81cff716acd89cc32ea2c03f58acd8f75
matildkushner <matildakushner@outlook.com.au>
2025-06-30 13:13:17 +1000

    Update emoji-float.js

--- emoji-float.js
+++ emoji-float.js
@@ -1,18 +1,1 @@
-const emojis = ['🦋', '✨', '❤️', '🧁', '🍒'];
 
-function createEmoji() {
-  const emoji = document.createElement('div');
-  emoji.classList.add('floating-emoji');
-  emoji.style.left = `${Math.random() * 100}vw`;
-  emoji.style.animationDuration = `${5 + Math.random() * 5}s`;
-  emoji.style.fontSize = `${20 + Math.random() * 15}px`;
-  emoji.style.zIndex = Math.random() > 0.5 ? '5' : '0';
-  emoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
-  document.body.appendChild(emoji);
-
-  setTimeout(() => {
-    emoji.remove();
-  }, 10000);
-}
-
-setInterval(createEmoji, 400);
